972bbf7aeb2c36ba68d6a256f4e6c422831517e4
Asher Oto <github@asheroto.com>
2021-06-01 00:46:48 -0500

    Update

---
+++ masscan/README.md
@@ -0,0 +1,3 @@
+This is a repository for the deployment of [masscan](https://github.com/robertdavidgraham/masscan) with [Chocolatey](https://chocolatey.org/).
+
+Chocolatey Package URL: https://community.chocolatey.org/packages/masscan
---
+++ masscan/tools/Chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$bits           = Get-ProcessorBits
+$toolsPath      = Split-Path $MyInvocation.MyCommand.Definition
+$binary32       = 'masscan32.exe'
+$binary64       = 'masscan64.exe'
+$binaryDefault  = 'masscan.exe' 
+
+if($bits -eq 64) {
+    Copy-Item -Path "$toolsPath\$binary64" -Destination "$toolsPath\$binaryDefault" -Force
+} else {
+    Copy-Item -Path "$toolsPath\$binary32" -Destination "$toolsPath\$binaryDefault" -Force
+}
