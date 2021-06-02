6cec827b0bb605c5d488315f522157db0938068c
Asher Oto <github@asheroto.com>
2021-06-02 18:59:44 -0500

    Add NVIDIA Data Center Driver

---
+++ nvidia-data-center-driver/README.md
@@ -0,0 +1,3 @@
+This is a repository for the deployment of NVIDIA Data Center Driver with [Chocolatey](https://chocolatey.org/).
+
+Chocolatey Package URL: https://community.chocolatey.org/packages/nvidia-data-center-driver
---
+++ nvidia-data-center-driver/tools/ChocolateyInstall.ps1
@@ -0,0 +1,23 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName    = 'nvidia-data-center-driver'
+$softwareName   = 'NVIDIA Data Center Driver*'
+$toolsPath      = Split-Path $MyInvocation.MyCommand.Definition
+$url            = 'https://us.download.nvidia.com/tesla/462.31/462.31-data-center-tesla-desktop-winserver-2019-2016-international.exe' 
+$checksum       = '92da4be08254a43cba5d7ba7c7f86d5c44cfc5bbf5e5537a8f378e31f96bc34f'		
+$silentArgs     = '-s'
+$validExitCodes = @(0)
+$fileLocation   = "$toolsPath\NVIDIA_Data_Center_Driver.exe"
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'exe'
+  file          = $fileLocation
+  url           = $url
+  checksum      = $checksum
+  checksumType  = 'sha256'
+  silentArgs    = $silentArgs
+  validExitCodes= $validExitCodes
+  softwareName  = $softwareName
+}
+ 
+Install-ChocolateyPackage @packageArgs
