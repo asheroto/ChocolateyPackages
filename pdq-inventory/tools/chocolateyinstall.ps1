19e39ece0cf4777f0fbe7089d78666f8cfa6951e
Asher Oto <49938263+asheroto@users.noreply.github.com>
2021-07-14 17:00:52 -0500

    Switch PDQ Inventory to included EXE

--- pdq-inventory/tools/chocolateyinstall.ps1
+++ pdq-inventory/tools/chocolateyinstall.ps1
@@ -1,18 +1,17 @@
-﻿$ErrorActionPreference = 'Stop'
+﻿# URL: http://www2.adminarsenal.com/download-pdqinventory
+$ErrorActionPreference = 'Stop'
 $packageName    = 'pdq-inventory'
 $softwareName   = 'PDQ Inventory*'
 $toolsPath      = Split-Path $MyInvocation.MyCommand.Definition
-$url            = 'http://www2.adminarsenal.com/download-pdqinventory' 
-$checksum       = 'f0cab15bc6f443e629ece757abd98b9c3fd96a634b7aae5655a9c415620ba9f8'		
+$checksum       = 'ebf79ee948a375a833cdf37bb867c4943a7ad8eb41d575a8b163ba7f3af03a4e'		
 $silentArgs     = '/s'
 $validExitCodes = @(0)
-$fileLocation   = "$toolsPath\Inventory.19.3.42.0.exe"
+$fileLocation   = "$toolsPath\Inventory_19.3.48.0.exe"
 
 $packageArgs = @{
   packageName   = $packageName
   fileType      = 'exe'
   file          = $fileLocation
-  url           = $url
   checksum      = $checksum
   checksumType  = 'sha256'
   silentArgs    = $silentArgs
