97126f3d9f3ff399a3caf57add9863251e73eba1
Asher Oto <49938263+asheroto@users.noreply.github.com>
2021-08-03 18:26:00 -0500

    Add Metadata++

---
+++ metadataplusplus/tools/ChocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName    = 'metadataplusplus'
+$softwareName   = 'Metadata++*'
+$toolsPath      = Split-Path $MyInvocation.MyCommand.Definition
+$checksum       = 'f999cb7c7935f4e9e4b6f4b1420dcebaaef242e29f0ce1c2f2bef569273eca67'		
+$silentArgs     = '/verysilent'
+$validExitCodes = @(0)
+$fileLocation   = "$toolsPath\metadata++-2-00-4.exe"
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'exe'
+  file          = $fileLocation
+  checksum      = $checksum
+  checksumType  = 'sha256'
+  silentArgs    = $silentArgs
+  validExitCodes= $validExitCodes
+  softwareName  = $softwareName
+}
+ 
+Install-ChocolateyPackage @packageArgs
