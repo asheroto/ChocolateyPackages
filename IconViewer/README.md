5b24415bd5fb703edf65b5af0e28327c17083fc4
Asher Oto <49938263+asheroto@users.noreply.github.com>
2021-08-05 12:22:37 -0500

    Add IconViewer

---
+++ IconViewer/README.md
@@ -0,0 +1,3 @@
+This is a repository for the deployment of [IconViewer](https://www.botproductions.com/iconview/iconview.html) with [Chocolatey](https://chocolatey.org/).
+
+Chocolatey Package URL: https://community.chocolatey.org/packages/IconViewer
---
+++ IconViewer/tools/ChocolateyInstall.ps1
@@ -0,0 +1,26 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName = 'iconviewer'
+$softwareName = 'IconViewer*'
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+$checksum = '2eb365ea3e2f20848206b0b1835c58cc71e2aca1323a19319896b3c8e3bf3956'		
+$checksum64 = '3be3664cfac0b9270da161c2c4c323499b4fe40a8e68a2d34cd4425b12ef223f'		
+$silentArgs = '/quiet'
+$validExitCodes = @(0)
+$fileLocation = "$toolsPath\IconViewer3.02-Setup-x86.exe"
+$fileLocation64 = "$toolsPath\IconViewer3.02-Setup-x64.exe"
+
+$packageArgs = @{
+    packageName    = $packageName
+    fileType       = 'exe'
+    file           = $fileLocation
+    file64         = $fileLocation64
+    checksum       = $checksum
+    checksum64     = $checksum64
+    checksumType   = 'sha256'
+    checksumType64 = 'sha256'
+    silentArgs     = $silentArgs
+    validExitCodes = $validExitCodes
+    softwareName   = $softwareName
+}
+ 
+Install-ChocolateyPackage @packageArgs
