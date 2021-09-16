4f9048b32761a7ff58ff23a23db7b965c6d8a178
Asher Oto <49938263+asheroto@users.noreply.github.com>
2021-09-16 16:45:29 -0500

    Update IconViewer

--- IconViewer/tools/ChocolateyInstall.ps1
+++ IconViewer/tools/ChocolateyInstall.ps1
@@ -1,26 +1,21 @@
-﻿$ErrorActionPreference = 'Stop'
-$packageName = 'iconviewer'
-$softwareName = 'IconViewer*'
-$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
-$checksum = '2eb365ea3e2f20848206b0b1835c58cc71e2aca1323a19319896b3c8e3bf3956'		
-$checksum64 = '3be3664cfac0b9270da161c2c4c323499b4fe40a8e68a2d34cd4425b12ef223f'		
-$silentArgs = '/quiet'
-$validExitCodes = @(0)
-$fileLocation = "$toolsPath\IconViewer3.02-Setup-x86.exe"
-$fileLocation64 = "$toolsPath\IconViewer3.02-Setup-x64.exe"
+﻿$ErrorActionPreference 	= 'Stop'
+$packageName 			= 'iconviewer'
+$softwareName 			= 'IconViewer*'
+$silentArgs 			= '/quiet'
+$toolsPath      		= "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$fileLocationX86		= "$toolsPath\IconViewer3.02-Setup-x86.exe"
+$fileLocationX64		= "$toolsPath\IconViewer3.02-Setup-x64.exe"
+$fileType 				= 'exe'
+$validExitCodes 		= @(0)
 
 $packageArgs = @{
-    packageName    = $packageName
-    fileType       = 'exe'
-    file           = $fileLocation
-    file64         = $fileLocation64
-    checksum       = $checksum
-    checksum64     = $checksum64
-    checksumType   = 'sha256'
-    checksumType64 = 'sha256'
-    silentArgs     = $silentArgs
-    validExitCodes = $validExitCodes
-    softwareName   = $softwareName
+	packageName    = $packageName
+	fileType       = $fileType
+	file           = $fileLocationX86
+	$file64        = $fileLocationX64
+	silentArgs     = $silentArgs
+	validExitCodes = $validExitCodes
+	softwareName   = $softwareName
 }
- 
-Install-ChocolateyPackage @packageArgs
+
+Install-ChocolateyInstallPackage @packageArgs
