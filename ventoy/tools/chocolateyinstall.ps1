bb086d78f7bac373a13638d2040e4510a1ee421f
Asher Oto <49938263+asheroto@users.noreply.github.com>
2021-10-22 06:24:57 -0500

    Update Ventoy to 1.0.56

--- ventoy/tools/chocolateyinstall.ps1
+++ ventoy/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@
 $ErrorActionPreference = 'Stop';
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileName32 = 'ventoy-1.0.54-windows.zip'
+$fileName32 = 'ventoy-1.0.56-windows.zip'
 $packageName = $env:ChocolateyPackageName
 $shortcutPath = [Environment]::GetFolderPath("Programs") + '\Ventoy.lnk'
 $unzipLocation = "$Env:LOCALAPPDATA\$packageName"
