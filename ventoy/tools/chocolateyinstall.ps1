732e57df591f11b693a48fdf42aa1c3c13cfa7fe
Asher Oto <49938263+asheroto@users.noreply.github.com>
2021-12-20 06:33:15 -0600

    Update ventoy

--- ventoy/tools/chocolateyinstall.ps1
+++ ventoy/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@
 $ErrorActionPreference = 'Stop';
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileName32 = 'ventoy-1.0.62-windows.zip'
+$fileName32 = 'ventoy-1.0.63-windows.zip'
 $packageName = $env:ChocolateyPackageName
 $shortcutPath = [Environment]::GetFolderPath("Programs") + '\Ventoy.lnk'
 $unzipLocation = "$Env:LOCALAPPDATA\$packageName"
