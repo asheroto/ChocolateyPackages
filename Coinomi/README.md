e930cb26f6c9fda52858886e28c05d78ed42c364
Asher Oto <49938263+asheroto@users.noreply.github.com>
2022-01-13 16:20:30 -0600

    Coinomi

---
+++ Coinomi/README.md
@@ -0,0 +1,3 @@
+This is a repository for the deployment of [Coinomi](https://www.coinomi.com) with [Chocolatey](https://chocolatey.org/).
+
+Chocolatey Package URL: https://community.chocolatey.org/packages/Coinomi
---
+++ Coinomi/tools/ChocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference  = 'Stop'
+$packageName    = 'Coinomi'
+$softwareName   = 'Coinomi Wallet'
+$url            = 'https://storage.coinomi.com/binaries/desktop/coinomi-wallet-1.3.0-win64.exe'
+$checksum       = 'e7bc8930166a0c0e70cc17de1fc83e7cbac864d684af05468a8e7f8426038cf7'
+$silentArgs     = '/VERYSILENT'
+$validExitCodes = @(0)
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'exe'
+  file          = $fileLocation
+  url           = $url
+  checksum      = $checksum
+  checksumType  = 'sha256'
+  silentArgs    = $silentArgs
+  validExitCodes= $validExitCodes
+  softwareName  = $softwareName
+}
+
+Install-ChocolateyPackage @packageArgs
