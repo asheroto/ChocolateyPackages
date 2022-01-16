21ff9d1aeb3f9da5138c8b6e01efd89e8bb22505
Asher Oto <49938263+asheroto@users.noreply.github.com>
2022-01-16 02:32:28 -0600

    TablePlus

---
+++ TablePlus/tools/ChocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference  = 'Stop'
+$packageName    = 'sftpgo'
+$softwareName   = 'SFTPGo'
+$url            = 'https://download.tableplus.com/windows/4.8.6/TablePlusSetup.exe'
+$checksum       = '7AA7F6BF8FF724C4135F967F1BC51198E97ACF9AF94D2FBFAE944AB0F1305797'
+$silentArgs     = '/VERYSILENT'
+$validExitCodes = @(0)
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'exe'
+  file          = $fileLocation
+  url           = $url
+  checksum      = $checksum
+  checksumType  = 'sha256'
+  silentArgs    = $silentArgs
+  validExitCodes= $validExitCodes
+  softwareName  = $softwareName
+}
+
+Install-ChocolateyPackage @packageArgs
--- masscan/README.md
+++
@@ -1,3 +0,0 @@
-This is a repository for the deployment of [masscan](https://github.com/robertdavidgraham/masscan) with [Chocolatey](https://chocolatey.org/).
-
-Chocolatey Package URL: https://community.chocolatey.org/packages/masscan
