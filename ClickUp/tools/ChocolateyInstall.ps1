48a7b1d6dcf01436f234ba19b247ccdfbe8b9825
Asher Oto <49938263+asheroto@users.noreply.github.com>
2022-01-19 07:11:45 -0600

    Update ClickUp

--- ClickUp/tools/ChocolateyInstall.ps1
+++ ClickUp/tools/ChocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference  = 'Stop'
 $packageName    = 'ClickUp'
 $softwareName   = 'ClickUp'
-$url            = 'https://github.com/asheroto/ClickUp/releases/download/0.0.2.0/ClickUp.exe'
-$checksum       = '68AA83DE321703200356C59AC878A673CDE5EDF56E665865803816F1ADE43269'
+$url            = 'https://github.com/asheroto/ClickUp/releases/download/0.0.2.1/ClickUp.exe'
+$checksum       = '419F76E15F5D545E72F1FDAE6D4757E2E9D6A867189F03A80926DA62E5CA2826'
 $silentArgs     = '/quiet'
 $validExitCodes = @(0)
 
