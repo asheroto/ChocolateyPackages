5f3689f7479feda96b30cbfcf7acdc4204843345
Asher Oto <49938263+asheroto@users.noreply.github.com>
2022-01-19 06:28:35 -0600

    Add ClickUp

---
+++ ClickUp/tools/ChocolateyInstall.ps1
@@ -0,0 +1,47 @@
+﻿$ErrorActionPreference  = 'Stop'
+$packageName    = 'sftpgo'
+$softwareName   = 'SFTPGo'
+$url            = 'https://github.com/drakkan/sftpgo/releases/download/v2.2.1/sftpgo_v2.2.1_windows_x86_64.exe'
+$checksum       = '9BA42FD43E3402165572D7A7C7F4E6CF10CEA927700F63EA714BA947DDE620F0'
+$silentArgs     = '/VERYSILENT'
+$validExitCodes = @(0)
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'exe'
+  file          = $fileLocation
+  url           = $url
+  checksum      = $checksum
+  checksumType  = 'sha256'
+  silentArgs    = $silentArgs
+  validExitCodes= $validExitCodes
+  softwareName  = $softwareName
+}
+
+Install-ChocolateyPackage @packageArgs
+
+$DefaultDataPath = Join-Path -Path $ENV:ProgramData -ChildPath "SFTPGo"
+$DefaultConfigurationFilePath = Join-Path -Path $DefaultDataPath -ChildPath "config.json"
+
+# `t = tab
+Write-Output "---------------------------"
+Write-Output ""
+Write-Output "If you have never used SFTPGo before, the web administration panel is located here:"
+Write-Output "`thttp://localhost:8080/web/admin"
+Write-Output ""
+Write-Output "Default web administration port:"
+Write-Output "`t8080"
+Write-Output "Default SFTP port:"
+Write-Output "`t2022"
+Write-Output ""
+Write-Output "Default data location:"
+Write-Output "`t$DefaultDataPath"
+Write-Output "Default configuration file location:"
+Write-Output "`t$DefaultConfigurationFilePath"
+Write-Output ""
+Write-Output "General information (README) location:"
+Write-Output "`thttps://github.com/drakkan/sftpgo"
+Write-Output "Detailed information (docs folder) location:"
+Write-Output "`thttps://github.com/drakkan/sftpgo/tree/main/docs"
+Write-Output ""
+Write-Output "---------------------------"
