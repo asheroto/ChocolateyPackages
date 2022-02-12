0fce3caf86e457357f1b313cd8c7ac05a7815326
asheroto <github@asheroto.com>
2022-02-12 13:11:55 -0600

    Update ventoy

--- ventoy/tools/chocolateyinstall.ps1
+++ ventoy/tools/chocolateyinstall.ps1
@@ -2,25 +2,26 @@
 # ventoy-$version
 # └── Ventoy2Disk.exe
 
-$ErrorActionPreference = 'Stop';
-
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileName32 = 'ventoy-1.0.65-windows.zip'
-$packageName = $env:ChocolateyPackageName
-$shortcutPath = [Environment]::GetFolderPath("Programs") + '\Ventoy.lnk'
-$unzipLocation = "$Env:LOCALAPPDATA\$packageName"
-$version = $env:ChocolateyPackageVersion
+$ErrorActionPreference	= 'Stop';
+$packageName			= "ventoy"
+$fileName				= "ventoy-1.0.65-windows.zip"
+$toolsDir				= $(Split-Path -Parent $MyInvocation.MyCommand.Definition)
+$file					= Join-Path $toolsDir $fileName
+$shortcutPath			= Join-Path ([Environment]::GetFolderPath("Programs")) "Ventoy.lnk"
+$unzipLocation			= Join-Path ([Environment]::GetFolderPath("LocalApplicationData")) $packageName
+$version				= [Environment]::GetEnvironmentVariable("ChocolateyPackageVersion")
 
 $packageArgs = @{
 	packageName		= $packageName
 	unzipLocation	= $unzipLocation
-	file			= "$toolsDir\$fileName32"
+	file			= $file
 }
 
 Install-ChocolateyZipPackage @packageArgs
 
-Copy-Item -Path "$unzipLocation/ventoy-$version/*" -Destination $unzipLocation -Force -Recurse -ErrorAction SilentlyContinue
-Remove-Item "$unzipLocation/ventoy-$version" -Force -Recurse -ErrorAction SilentlyContinue
+# Keeping this logic as is - was from original maintainer script
+Copy-Item -Path "$unzipLocation\ventoy-$version\*" -Destination $unzipLocation -Force -Recurse -ErrorAction SilentlyContinue
+Remove-Item "$unzipLocation\ventoy-$version" -Force -Recurse -ErrorAction SilentlyContinue
 
-$exePath = (Get-ChildItem $unzipLocation -Filter "Ventoy2Disk.exe" -Recurse).FullName
-Install-ChocolateyShortcut -shortcutFilePath $shortcutPath -Target "$exePath" -WorkingDirectory $unzipLocation
+$exePath = Join-Path $unzipLocation "Ventoy2Disk.exe"
+Install-ChocolateyShortcut -ShortcutFilePath $shortcutPath -Target $exePath -WorkingDirectory $unzipLocation
--- ventoy/tools/chocolateyuninstall.ps1
+++ ventoy/tools/chocolateyuninstall.ps1
@@ -1,9 +1,10 @@
-﻿$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference	= 'Stop';
 
-$shortcutsPath = Join-Path ([Environment]::GetFolderPath("Programs")) 'Ventoy.lnk'
-$unzipPath = "$Env:LOCALAPPDATA\$env:ChocolateyPackageName"
+$shortcutsPath			= Join-Path ([Environment]::GetFolderPath("Programs")) 'Ventoy.lnk'
+$unzipPath				= Join-Path ([Environment]::GetFolderPath("LocalApplicationData")) ([Environment]::GetEnvironmentVariable("ChocolateyPackageName"))
 
 Remove-Item -Path $shortcutsPath -ErrorAction SilentlyContinue
+
 if (Test-Path $unzipPath) { 
 	Remove-Item -Path $unzipPath -Recurse -Force -ErrorAction SilentlyContinue
 }
