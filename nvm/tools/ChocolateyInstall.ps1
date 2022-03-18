87103057415203f72ab21b17cab6177c2d54f96b
asheroto <github@asheroto.com>
2022-03-18 01:34:30 -0500

    Update nvm

--- nvm/tools/ChocolateyInstall.ps1
+++ nvm/tools/ChocolateyInstall.ps1
@@ -1,21 +1,19 @@
 ﻿$ErrorActionPreference	= 'Stop'
 $packageName			= 'nvm'
-$softwareName			= 'nvm*'
 $toolsPath				= Split-Path $MyInvocation.MyCommand.Definition
-$checksum				= '4B16E9289A41527F17ECA80C745688C6AF6D10A882294BF94B2ED71C80FA42EF'
-$silentArgs				= '/verysilent /norestart'
-$validExitCodes			= @(0)
+$silentArgs				= '/verysilent /norestart /suppressmsgboxes'
 $fileLocation			= "$toolsPath\nvm-setup.exe"
+$url					= "https://github.com/coreybutler/nvm-windows/releases/download/1.1.9/nvm-setup.zip"
+$checksum				= "93c135ba59f13b7138a8216b3ef03d88f88d1e3e8420ccbb681244a9070aa475"
 
 $packageArgs = @{
-	packageName    = $packageName
-	fileType       = 'exe'
-	file           = $fileLocation
-	checksum       = $checksum
-	checksumType   = 'sha256'
-	silentArgs     = $silentArgs
-	validExitCodes = $validExitCodes
-	softwareName   = $softwareName
+  packageName   = $packageName
+  fileType      = 'zip'
+  url           = $url
+  checksum      = $checksum
+  checksumType  = 'sha256'
+  unzipLocation = $toolsPath
 }
- 
-Install-ChocolateyPackage @packageArgs
+
+Install-ChocolateyZipPackage @packageArgs
+Start-Process $fileLocation -ArgumentList $silentArgs
