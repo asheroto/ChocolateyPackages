659a22d76c547ac2ed7ab3d4ed85541b832f0e07
asheroto <github@asheroto.com>
2022-03-26 22:47:17 -0500

    Update ventoy

--- ventoy/tools/chocolateyinstall.ps1
+++ ventoy/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@
 
 $ErrorActionPreference	= 'Stop';
 $packageName = "ventoy"
-$fileName = "ventoy-1.0.71-windows.zip"
+$fileName = "ventoy-1.0.72-windows.zip"
 $toolsDir = $(Split-Path -Parent $MyInvocation.MyCommand.Definition)
 $file = Join-Path $toolsDir $fileName
 $shortcutPath = Join-Path ([Environment]::GetFolderPath("Programs")) "Ventoy.lnk"
