2b70db7cb679c929b04d5ca6be50c07a150f6cad
asheroto <github@asheroto.com>
2022-12-29 01:54:02 -0600

    Update Metadata++

--- metadataplusplus/tools/ChocolateyInstall.ps1
+++ metadataplusplus/tools/ChocolateyInstall.ps1
@@ -1,19 +1,21 @@
-﻿$ErrorActionPreference 	= 'Stop'
-$packageName 			= 'metadataplusplus'
-$softwareName 			= 'Metadata++*'
-$silentArgs 			= '/verysilent'
-$toolsPath      		= "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-$fileLocation 			= "$toolsPath\metadata++-2-00-4.exe"
-$fileType 				= 'exe'
-$validExitCodes 		= @(0)
+﻿$ErrorActionPreference = 'Stop'
+$packageName    = 'metadataplusplus'
+$softwareName   = 'Metadata++*'
+$toolsPath      = Split-Path $MyInvocation.MyCommand.Definition
+$checksum       = '367A18C7270C0E7BF8FE060803EB068E7D85238D5EBB117B93669C545DF421FB'
+$silentArgs     = '/verysilent'
+$validExitCodes = @(0)
+$fileLocation   = "$toolsPath\metadata++-2-02-3.exe"
 
 $packageArgs = @{
-	packageName    = $packageName
-	softwareName   = $softwareName
-	silentArgs     = $silentArgs
-	file           = $fileLocation
-	fileType       = $fileType
-	validExitCodes = $validExitCodes
+  packageName   = $packageName
+  fileType      = 'exe'
+  file          = $fileLocation
+  checksum      = $checksum
+  checksumType  = 'sha256'
+  silentArgs    = $silentArgs
+  validExitCodes= $validExitCodes
+  softwareName  = $softwareName
 }
 
-Install-ChocolateyInstallPackage $packageArgs
+Install-ChocolateyPackage @packageArgs
