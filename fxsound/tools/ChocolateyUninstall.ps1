cab95e04e5642b6d287883b60e6d400dd2aae9b5
asheroto <github@asheroto.com>
2023-05-13 22:48:20 -0500

    Update FxSound

--- fxsound/tools/ChocolateyUninstall.ps1
+++ fxsound/tools/ChocolateyUninstall.ps1
@@ -1,4 +1,7 @@
 ﻿Write-Output "---------------------------"
+Write-Output ""
 Write-Output "Uninstalling FxSound"
-Write-Output "YOU MAY NEED TO RESTART YOUR COMPUTER TO RESUME NORMAL AUDIO FUNCTIONALITY AFTER UNINSTALLING"
+Write-Output ""
+Write-Output "YOU MAY NEED TO RESTART YOUR COMPUTER TO RESUME NORMAL AUDIO FUNCTIONALITY"
+Write-Output ""
 Write-Output "---------------------------"
