e85937a0054745a0db7d6becfab5353aae079dbe
asheroto <github@asheroto.com>
2023-06-08 18:36:35 -0500

    Add Intel RST Driver

---
+++ intel-rst-driver/README.md
@@ -0,0 +1,3 @@
+This is a repository for the deployment of [Intel Rapid Storage Technology Driver](https://www.intel.com/content/www/us/en/download/19512/intel-rapid-storage-technology-driver-installation-software-with-intel-optane-memory-10th-and-11th-gen-platforms.html) with [Chocolatey](https://chocolatey.org/).
+
+Chocolatey Package URL: https://community.chocolatey.org/packages/intel-rst-driver
---
+++ intel-rst-driver/tools/ChocolateyInstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName = 'intel-rst-driver' 
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://downloadmirror.intel.com/773229/SetupRST.exe'
+#                                                ^^^^^ is what changes with the version
+$checksum = 'A2B2E20D6D8100E9EE344746F80849524C64490B90686A13C09268CADB976B37'
+
+$packageArgs = @{
+	packageName    = $packageName
+	unzipLocation  = $toolsDir
+	fileType       = 'EXE'
+	url            = $url
+	checksum       = $checksum
+	checksumType   = 'sha256'
+	silentArgs     = '-s /acceptEULA'
+	softwareName   = 'Intel(R) Rapid Storage Technology'
+	validExitCodes = @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
