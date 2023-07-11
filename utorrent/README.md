61c61ab7b5b55f9f35cb317a7c16459722f7682b
asheroto <github@asheroto.com>
2023-07-11 17:37:30 -0500

    Add readme for tableplus, add utorrent

---
+++ utorrent/README.md
@@ -0,0 +1,3 @@
+This is a repository for the deployment of [uTorrent Classic](https://www.utorrent.com/) with [Chocolatey](https://chocolatey.org/).
+
+Chocolatey Package URL: https://community.chocolatey.org/packages/uTorrent
---
+++ utorrent/tools/chocolateyinstall.ps1
@@ -0,0 +1,35 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName    = 'utorrent'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url            = 'http://download.ap.bittorrent.com/track/stable/endpoint/utorrent/os/windows'
+$checksum       = 'D5FFE0BF0F8676C7EF6AD4A1E84EC22580BD9224C1B91F8F54BEFC330CAD9E6B'
+$extractDir     = "$toolsDir\extracted"
+$fileLocation   = "$extractDir\Carrier.exe"
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $extractDir
+  fileType      = 'ZIP' 
+  url           = $url
+  checksum      = $checksum
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'EXE'
+  file           = $fileLocation
+  silentArgs     = '/S'
+  validExitCodes = @(0,1)
+  softwareName   = 'uTorrent*'
+}
+ 
+Start-CheckandStop "uTorrent"
+
+Install-ChocolateyPackage @packageArgs
+
+Start-Sleep -s 10
+if ($ProcessWasRunning -eq "True") {$ProcessFullPath} else {Start-CheckandStop "uTorrent"}
+Remove-Item $extractDir -Recurse -Force -EA SilentlyContinue | Out-Null
