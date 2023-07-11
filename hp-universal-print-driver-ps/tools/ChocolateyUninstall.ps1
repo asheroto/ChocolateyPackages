a2954c6384d04970d6486a0fbc34c78c4cb7ff73
asheroto <github@asheroto.com>
2023-07-11 16:45:34 -0500

    Update hp drivers

---
+++ hp-universal-print-driver-ps/tools/ChocolateyUninstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop'
+
+# Define driver name
+$driverName = 'HP Universal Printing PS'
+
+# Restart spooler service to avoid "The specified printer driver is currently in use" error
+Restart-Service -Name Spooler -Force
+
+# Remove printers using that driver
+Get-Printer | ? { $_.DriverName -eq $driverName } | Remove-Printer
+
+# Remove driver
+Remove-PrinterDriver "HP Universal Printing PS"
+
+# Notify user that the driver has been removed
+Write-Output "---------------------------"
+Write-Output ""
+Write-Output "Printers using the $driverName driver have been removed. This is required to remove the driver."
+Write-Output ""
+Write-Output "The $driveName driver have been removed."
+Write-Output ""
+Write-Output "---------------------------"
