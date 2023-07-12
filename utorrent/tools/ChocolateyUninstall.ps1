1b8674b5d7906c2b1c6d066f185cf3eaf36ae3d6
asheroto <github@asheroto.com>
2023-07-12 16:15:55 -0500

    Fix uTorrent spacing

--- utorrent/tools/ChocolateyUninstall.ps1
+++ utorrent/tools/ChocolateyUninstall.ps1
@@ -1,10 +1,10 @@
 ﻿# Define vars
-$ErrorActionPreference 	= "Stop"
-$packageName    		= "utorrent"
-$fileType				= "exe"
-$silentArgs     		= "/uninstall /S"
-$validExitCodes 		= @(0, 3010, 1605, 1614, 1641)
-$uTorrentPath 			= "$env:APPDATA\uTorrent\uTorrent.exe"
+$ErrorActionPreference  = "Stop"
+$packageName            = "utorrent"
+$fileType               = "exe"
+$silentArgs             = "/uninstall /S"
+$validExitCodes         = @(0, 3010, 1605, 1614, 1641)
+$uTorrentPath           = "$env:APPDATA\uTorrent\uTorrent.exe"
 
 # Define package args
 $packageArgs = @{
