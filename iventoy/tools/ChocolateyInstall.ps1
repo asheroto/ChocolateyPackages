4dcf615c9eacb3469cb6b19c300c4bc820ae9592
asheroto <github@asheroto.com>
2023-07-17 18:37:35 -0500

    Update iVentoy, fix uTorrent nuspec

--- iventoy/tools/ChocolateyInstall.ps1
+++ iventoy/tools/ChocolateyInstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference	= 'Stop';
 
 $packageName 	= "iventoy"
-$version 		= "1.0.11" # Chocolatey package version may differ from the filename version
-$url 			= 'https://github.com/ventoy/PXE/releases/download/v1.0.11/iventoy-1.0.11-win32-free.zip'
-$checksum 		= 'B442DEF627A9585C83141CE6DA5A7D961D6CEB948A1B8A2FF8DDAC05D14F1D01'
-$url64 			= 'https://github.com/ventoy/PXE/releases/download/v1.0.11/iventoy-1.0.11-win64-free.zip'
-$checksum64 	= '109C6A47B75D0A9DDAA4050CF40CF5F22978330E49BFF18EC461F4C4964FEE73'
+$version 		= "1.0.12" # Chocolatey package version may differ from the filename version
+$url 			= 'https://github.com/ventoy/PXE/releases/download/v1.0.12/iventoy-1.0.12-win32-free.zip'
+$checksum 		= 'A0C815631D00D82F720D7330CCDDA50EEBF0508B49873623E81A9DE5CD0CD5F6'
+$url64 			= 'https://github.com/ventoy/PXE/releases/download/v1.0.12/iventoy-1.0.12-win64-free.zip'
+$checksum64 	= '15D3C9A661CC61A5104BEEEBA1D012C45D8841DDE30E780EB2A03443164E0E10'
 $unzipLocation 	= Join-Path ([Environment]::GetFolderPath("LocalApplicationData")) $packageName
 
 $packageArgs = @{
