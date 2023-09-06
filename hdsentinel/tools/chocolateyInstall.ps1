43e485bb195fdb5ef7ed7a2964f8b63211c54b4a
asheroto <github@asheroto.com>
2023-09-06 03:48:32 -0500

    Update hdsentinel

--- hdsentinel/tools/chocolateyInstall.ps1
+++ hdsentinel/tools/chocolateyInstall.ps1
@@ -1,30 +1,26 @@
-﻿. (Join-Path (Split-Path -parent $MyInvocation.MyCommand.Definition) 'helpers\KillAsap.ps1')
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 
-$ErrorActionPreference = 'Stop'; # stop on all errors
+# https://www.hdsentinel.com/download.php
+. (Join-Path (Split-Path -parent $MyInvocation.MyCommand.Definition) 'helpers\KillAsap.ps1')
 
-$packageName    = "hdsentinel"
-$packageTitle   = "HDSentinel"
-$installerType  = "EXE"
+$packageName = "hdsentinel"
+$packageTitle = "HDSentinel"
+$installerType = "EXE"
 $packageVersion = "6.10.4"
-$url            = "http://www.harddisksentinel.com/hdsentinel_setup.zip"
-$silentArgs     = "/sp- /verysilent /norestart"
-$checksum       = "1065e2aa48d0f681fb223615ea773ec1149abaa9ab9f4ccec9897f16237c3852"
-$checksumType   = "sha256"
+$url = "https://www.harddisksentinel.com/hdsentinel_setup.zip"
+$silentArgs = "/sp- /verysilent /norestart"
+$checksum = "27B23007859B0D535106B508FEB9B79FEBFA2F891CFF443DAA0418CA4CB85B5B"
+$checksumType = "sha256"
 $validExitCodes = @(0)
 
-
-
 #extract filename from source URL
-$filename       = $url.Substring($url.LastIndexOf("/") + 1)
+$filename = $url.Substring($url.LastIndexOf("/") + 1)
 
 #establish temp folder path
-$tmpDir         = $env:temp,$packageName -join "\"
+$tmpDir = $env:temp, $packageName -join "\"
 
 #establish full path to local copy of downloaded zip file
-$pathToZip      = ($tmpDir,$filename -join "\")
-
-
-
+$pathToZip = ($tmpDir, $filename -join "\")
 
 # download zip package
 Get-ChocolateyWebFile  `
@@ -34,29 +30,21 @@ Get-ChocolateyWebFile  `
     -Checksum $checksum `
     -ChecksumType $checksumType
 
-
-
 # extract it
 Get-ChocolateyUnzip `
-  -FileFullPath $pathToZip `
-  -Destination $tmpDir
-
-
+    -FileFullPath $pathToZip `
+    -Destination $tmpDir
 
 # establish path to extracted installer (exe)
-$pathToExe =  Join-Path $tmpDir (get-childitem $tmpDir | where {$_.extension -eq ".exe"}).Name
-
-
+$pathToExe = Join-Path $tmpDir (get-childitem $tmpDir | where { $_.extension -eq ".exe" }).Name
 
 # install package
 Install-ChocolateyInstallPackage `
-  -PackageName $packageName `
-  -FileType $installerType `
-  -SilentArgs $silentArgs `
-  -ValidExitCodes $validExitCodes `
-  -File $pathToExe
-
-
+    -PackageName $packageName `
+    -FileType $installerType `
+    -SilentArgs $silentArgs `
+    -ValidExitCodes $validExitCodes `
+    -File $pathToExe
 
 # the app spins up a window straight after installation ends. Kill it.
 KillAsap $packageTitle
