57e844126b2f4198ac18342b66238f88f8cca72a
asheroto <github@asheroto.com>
2023-09-26 15:04:51 -0500

    Add update.ps1 to ClickUp-Official to monitor version changes in EXE

---
+++ ClickUp-Official/update.ps1
@@ -0,0 +1,107 @@
+$ErrorActionPreference = "Stop"
+
+# Initialize variables
+$FILE_URL = 'https://desktop.clickup.com/windows'
+$downloadPath = '.\ClickUp-Temp.exe'
+$nuspecPath = '.\ClickUp-Official.nuspec'
+$installScriptPath = '.\tools\ChocolateyInstall.ps1'
+
+function SendAlert {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$Subject,
+
+        [Parameter(Mandatory = $true)]
+        [string]$Message
+    )
+
+    # Get the secret URL from the secure string file using the path in the environment variable
+    $CredsFile = [System.Environment]::GetEnvironmentVariable('EMAIL_NOTIFICATION_CREDS_PATH', [System.EnvironmentVariableTarget]::User)
+
+    # Get the password from the file and convert to plaintext
+    $secret = Get-Content $CredsFile | ConvertTo-SecureString
+    $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($secret)
+    $alertUrl = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+
+    # Replace {SUBJECT} and {MESSAGE} in the URL
+    $alertUrl = $alertUrl -replace '{SUBJECT}', $Subject
+    $alertUrl = $alertUrl -replace '{MESSAGE}', $Message
+
+    if ($alertUrl) {
+        Invoke-WebRequest -Uri $alertUrl -Method Post -Body $Message -ContentType "text/plain" | Out-Null
+        Write-Output "Alert sent."
+    }
+}
+
+function SendPackageUpdateAlert {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$Package,
+
+        [Parameter(Mandatory = $true)]
+        [string]$Version
+    )
+
+    # Create the HTML body for the notification
+    $date = Get-Date -Format "yyyy-MM-dd hh:mm:ss tt"
+    $body = "<html><body>"
+    $body += "<font face='Arial'>"
+    $body += "<p>$Package has been updated to version $Version.</p>"
+    $body += "<p><strong>Time:</strong> $date</p>"
+    $body += "</font>"
+    $body += "</body></html>"
+
+    SendAlert -Subject "$Package Package Updated" -Message $body
+}
+
+try {
+    # Download the file and get its ProductVersion
+    Invoke-WebRequest -Uri $FILE_URL -OutFile $downloadPath
+    $productVersion = (Get-Command $downloadPath).FileVersionInfo.ProductVersion
+
+    # Trim the version to 3 parts if it has 4
+    if ($productVersion -match '^\d+(\.\d+){3}$') {
+        $productVersion = ($productVersion -split '\.')[0..2] -join '.'
+    }
+
+    # Get the current version from the nuspec
+    $nuspecContent = Get-Content $nuspecPath -Raw
+    $versionMatches = [regex]::Match($nuspecContent, '<version>(.*?)<\/version>')
+    $nuspecVersion = $versionMatches.Groups[1].Value
+
+    # Get the current checksum from the ps1 file
+    $installScriptContent = Get-Content $installScriptPath -Raw
+    $checksumMatches = [regex]::Match($installScriptContent, '\$checksum\s*=\s*"([^"]+)"')
+    $currentChecksum = $checksumMatches.Groups[1].Value
+
+    # Calculate the new checksum
+    $newChecksum = (Get-FileHash -Algorithm SHA256 $downloadPath).Hash
+
+    # Validate version strings
+    if ($productVersion -match '^\d+(\.\d+){1,3}$' -and $nuspecVersion -match '^\d+(\.\d+){1,3}$') {
+        # Compare versions and checksums
+        if ([version]$productVersion -gt [version]$nuspecVersion -or $newChecksum -ne $currentChecksum) {
+            # Update the nuspec version using regex
+            $nuspecContent = $nuspecContent -replace '(<version>).*?(<\/version>)', "`${1}$productVersion`$2"
+            Set-Content -Path $nuspecPath -Value $nuspecContent
+
+            # Update the checksum in ChocolateyInstall.ps1
+            $updatedInstallScriptContent = $installScriptContent -replace '(\$checksum\s*=\s*)".*"', "`$1`"$newChecksum`""
+            Set-Content -Path $installScriptPath -Value $updatedInstallScriptContent
+
+            Write-Output "Package updated."
+            SendPackageUpdateAlert -Package "ClickUp" -Version $productVersion
+        } else {
+            Write-Output "No update needed."
+        }
+    } else {
+        Write-Output "Invalid version format. Skipping update."
+    }
+} catch {
+    Write-Output "An error occurred: $_"
+} finally {
+    # Clean up the downloaded file
+    Remove-Item -Path $downloadPath -ErrorAction SilentlyContinue
+}
