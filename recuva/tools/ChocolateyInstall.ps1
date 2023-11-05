b811524459b8adefc920ca029705b46a330de985
asheroto <github@asheroto.com>
2023-11-05 02:38:17 -0600

    Update

--- recuva/tools/ChocolateyInstall.ps1
+++ recuva/tools/ChocolateyInstall.ps1
@@ -10,9 +10,9 @@ if ($Env:ChocolateyPackageParameters -match '/UseSystemLocale') {
 $packageArgs = @{
     PackageName    = "recuva"
     SoftwareName   = "Recuva"
-    Version        = "1.53.2096" # Leave for update.ps1
-    Url            = "https://download.ccleaner.com/rcsetup153.exe" # URL does not change with Recuva
-    Checksum       = "B3DF198D64BA6F401611F56743BD344C1B02915F9E5D571D271EF8557FEAF56C"  # Leave for update.ps1
+    Version        = "1.53.2096"                                                            # Leave for update.ps1
+    Url            = "https://download.ccleaner.com/rcsetup153.exe"                         # URL does not change with Recuva
+    Checksum       = "B3DF198D64BA6F401611F56743BD344C1B02915F9E5D571D271EF8557FEAF56C"     # Leave for update.ps1
     ChecksumType   = "sha256"
     SilentArgs     = "/S $locale"
     ValidExitCodes = @(0)
