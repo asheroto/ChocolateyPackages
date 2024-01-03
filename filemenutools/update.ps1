c47142672aa1754b4c0f21f38f08daca12dfc1a9
asheroto <github@asheroto.com>
2024-01-02 22:26:08 -0600

    Update FileMenuTools

--- filemenutools/update.ps1
+++ filemenutools/update.ps1
@@ -15,15 +15,9 @@ Set-Location $scriptPath
 
 # Create a hash table to store package information
 $packageInfo = @{
-    PackageName            = "filemenutools"
-    FileUrl                = 'https://www.lopesoft.com/fmtools/FileMenuTools-setup.exe'    # URL to download the file from
-    FileDownloadTempPath   = '.\FileMenuTools-setup-temp.exe'                              # Path to save the file to
-    FileDownloadTempDelete = $true                                                         # Delete the temporary file after downloading and comparing to exiting version & checksum
-    FileDestinationPath    = '.\tools\FileMenuTools-setup.exe'                             # Path to move/rename the temporary file to (if EXE is distributed in package)
-    NuspecPath             = '.\filemenutools.nuspec'                                      # Path to the nuspec file
-    InstallScriptPath      = '.\tools\ChocolateyInstall.ps1'                               # Path to the ChocolateyInstall.ps1 script
-    VerificationPath       = '.\tools\VERIFICATION.txt'                                    # Path to the VERIFICATION.txt file
-    Alert                  = $true                                                         # If the package is updated, send a message to the maintainer for review
+    PackageName          = "filemenutools"
+    FileUrl              = 'https://www.lopesoft.com/fmtools/FileMenuTools-setup.exe'    # URL to download the file from
+    FileDestinationPath  = '.\tools\FileMenuTools-setup.exe'                             # Path to move/rename the temporary file to (if EXE is distributed in package)
 }
 
 # Call the UpdateChocolateyPackage function and pass the hash table
