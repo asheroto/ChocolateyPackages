c8a1686078a57fdc90c3587437e3ea602c3af448
asheroto <49938263+asheroto@users.noreply.github.com>
2024-03-14 12:52:50 -0500

    Update StartAllBack

--- StartAllBack/tools/ChocolateyInstall.ps1
+++ StartAllBack/tools/ChocolateyInstall.ps1
@@ -2,14 +2,14 @@ $ErrorActionPreference = "Stop"
 
 # https://startallback.com/download.php
 
-$Version = "3.7.6"
+$Version = "3.7.7"
 
 # Package args
 $packageArgs = @{
     PackageName    = "StartAllBack"
     SoftwareName   = "StartAllBack"
     Url            = "https://startisback.sfo3.cdn.digitaloceanspaces.com/StartAllBack_${Version}_setup.exe"
-    Checksum       = "CC08B5E72B9C9802D5AED41AD6B464C9FA45CA0BF6CEEEE347C931E537D1FFCC"
+    Checksum       = "469CACB82B4558ABAC90D48B7FC040B1FDD4E835F4A875BADED6596BA922E7FC"
     ChecksumType   = "sha256"
     SilentArgs     = "/silent /elevated"
     ValidExitCodes = @(0)
