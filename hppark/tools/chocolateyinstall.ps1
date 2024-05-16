21d33c8bc787e25f0acef8bee9e6c9e3794ed418
asheroto <49938263+asheroto@users.noreply.github.com>
2024-05-16 12:35:15 -0500

    Fix shortcut, checksum

--- hppark/tools/chocolateyinstall.ps1
+++ hppark/tools/chocolateyinstall.ps1
@@ -1,7 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 # Download:
-# https://support.hp.com/us-en/drivers/supd/model/33835514
 # https://support.hp.com/us-en/drivers/upd/model/4157320
 # Using the link from the download button won't work for scraping the version number due to the HP website.
 # Using VisualPing to monitor the version as it supports clicking. Check ran weekly.
@@ -10,7 +9,6 @@
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $version      = "1.9.1"
 $shortcutName = 'HP PARK.lnk'
-$subfolder    = "HP PARK-v$ENV:ChocolateyPackageVersion"
 
 # Package arguments
 $packageArgs = @{
@@ -18,7 +16,7 @@ $packageArgs = @{
     unzipLocation = $toolsDir
     fileType      = 'ZIP'
     url           = "https://ftp.hp.com/pub/softlib/software13/printers/UPD/HP-PARK-v${VERSION}.zip"
-    checksum      = 'AE6F21CFDD6286FFC786BBE1A81E654F0420BC5DBD83EDC1C0104276A28B4C68'
+    checksum      = '0E9AD16CC1A9879E4963A9D994A9438F22997AD908B6567F359EDE379980ED5A'
     checksumType  = 'sha256'
 }
 
@@ -27,5 +25,4 @@ Install-ChocolateyZipPackage @packageArgs
 
 # Create a desktop shortcut
 $shortcutPath = [System.IO.Path]::Combine($ENV:Public, 'Desktop', $shortcutName)
-$targetPath = [System.IO.Path]::Combine($toolsDir, $subfolder)
-Install-ChocolateyShortcut -shortcutFilePath $shortcutPath -targetPath $targetPath
+Install-ChocolateyShortcut -shortcutFilePath $shortcutPath -targetPath $toolsDir
