8dbb763fb3249c73662851940c85e5adce922732
asheroto <49938263+asheroto@users.noreply.github.com>
2024-05-31 01:08:22 -0500

    Update

--- StartAllBack/tools/ChocolateyInstall.ps1
+++ StartAllBack/tools/ChocolateyInstall.ps1
@@ -2,14 +2,14 @@ $ErrorActionPreference = "Stop"
 
 # https://startallback.com/download.php
 
-$Version = "3.7.9"
+$Version = "3.7.10"
 
 # Package args
 $packageArgs = @{
     PackageName    = "StartAllBack"
     SoftwareName   = "StartAllBack"
     Url            = "https://startisback.sfo3.cdn.digitaloceanspaces.com/StartAllBack_${Version}_setup.exe"
-    Checksum       = "5040BB8ED046C5F9E3880458E0D70C0FB99BC7F1663F43870744C2C841B8BB2E"
+    Checksum       = "1BA1F1236AD8D317312B155B5D854D7E7846183171F78C084A3DBE04CD67B123"
     ChecksumType   = "sha256"
     SilentArgs     = "/silent /elevated"
     ValidExitCodes = @(0)
--- recuva/update.ps1
+++ recuva/update.ps1
@@ -1,4 +1,7 @@
-[CmdletBinding()] # Enables -Debug parameter for troubleshooting
+# Disable for now as the website has changed significantly
+# Switched to VisualPing to monitor changes
+
+<# [CmdletBinding()] # Enables -Debug parameter for troubleshooting
 param ()
 
 # Set vars to the script and the parent path
@@ -10,11 +13,11 @@ $ParentPath = Split-Path -Parent $ScriptPath
 
 # Create a hash table to store package information
 $packageInfo = @{
-    PackageName   = "recuva"                                                                                        # Package name
-    ScrapeUrl     = 'https://www.ccleaner.com/recuva/release-notes'                                                 # URL to scrape for version number
-    ScrapePattern = '(?<=<h6>v)[\d.]+'                                                                              # Regex pattern to match version number
-    FileUrl       = "https://download.ccleaner.com/rcsetup153.exe"                                                  # URL to download the file from
+    PackageName   = "recuva"
+    ScrapeUrl     = 'https://www.ccleaner.com/recuva/release-notes'
+    ScrapePattern = '(?<=<strong>Patch update )[\d.]+'
+    FileUrl       = "https://download.ccleaner.com/rcsetup153.exe"
 }
 
 # Call the UpdateChocolateyPackage function and pass the hash table
-UpdateChocolateyPackage @packageInfo
+UpdateChocolateyPackage @packageInfo #>
