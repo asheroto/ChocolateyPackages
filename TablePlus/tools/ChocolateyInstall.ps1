ef39830922819df3edafc4d2cdfddced92bcd27e
asheroto <49938263+asheroto@users.noreply.github.com>
2024-06-14 14:32:08 -0500

    Update tableplus, add httptoolkit

--- TablePlus/tools/ChocolateyInstall.ps1
+++ TablePlus/tools/ChocolateyInstall.ps1
@@ -1,12 +1,12 @@
 $ErrorActionPreference = "Stop"
 
-$version = "5.9.7"
+$version = "6.0.0"
 
 $packageArgs = @{
     packageName    = "TablePlus"
     fileType       = "exe"
     url            = "https://files.tableplus.com/windows/${version}/TablePlusSetup.exe"
-    checksum       = "9C952ED40B9A145B5B17682AFF8F128D2EF0046A87B7C40D7D4FAC2F29CCC41B"
+    checksum       = "D2D94A260196CD8E1747690A80F64DAA5F451B1C907220A9BD04A150E0F47684"
     checksumType   = "sha256"
     silentArgs     = "/VERYSILENT"
     validExitCodes = @(0)
