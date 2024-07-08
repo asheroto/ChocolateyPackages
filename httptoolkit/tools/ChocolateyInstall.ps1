7a6bed16ca9b0bb8ef598eed7008cd7a7ab97b7b
asheroto <49938263+asheroto@users.noreply.github.com>
2024-07-08 13:09:08 -0500

    Update

--- httptoolkit/tools/ChocolateyInstall.ps1
+++ httptoolkit/tools/ChocolateyInstall.ps1
@@ -1,9 +1,9 @@
 $ErrorActionPreference = 'Stop'
 $packageName = 'httptoolkit'
 $softwareName = 'httptoolkit'
-$version = "1.17.2"
+$version = "1.18.0"
 $url = "https://github.com/httptoolkit/httptoolkit-desktop/releases/download/v${VERSION}/HttpToolkit-installer-${VERSION}.exe"
-$checksum = 'EC46915433A8365A4F9358B2ACA3DBDA22DE05093CB2511C82FE1C492DF7D8C5'
+$checksum = '982DAE231F2702FAC9205D6572C12EDA3A9F0F5954A0D203C16C6E2C22C6E84A'
 $silentArgs = '/S'
 $validExitCodes = @(0)
 
