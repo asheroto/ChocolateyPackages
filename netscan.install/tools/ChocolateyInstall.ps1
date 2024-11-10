b02d2be47341afa2ca89f85e845718397b4a9c72
asheroto <49938263+asheroto@users.noreply.github.com>
2024-11-10 01:21:21 -0600

    Add message to netscan

--- netscan.install/tools/ChocolateyInstall.ps1
+++ netscan.install/tools/ChocolateyInstall.ps1
@@ -15,5 +15,12 @@ $packageArgs = @{
 # End process
 Get-Process netscan -ErrorAction SilentlyContinue | Stop-Process -ErrorAction Stop
 
+# Notify user about frequently changing checksum
+Write-Output ""
+Write-Warning "The checksum for this package often changes while the version number remains the same."
+Write-Output "While we try to keep this package up to date, you may still encounter errors if the checksum changes and the package is pending approval."
+Write-Output "If you experience checksum errors when installing, consider downloading the software from SoftPerfect's website, or as a less secure alternative, run with --ignore-checksums."
+Write-Output ""
+
 # Install
 Install-ChocolateyPackage @packageArgs
