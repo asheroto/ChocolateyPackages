a76332284c645514bdb31e45d30be7ff1a1f5043
asheroto <49938263+asheroto@users.noreply.github.com>
2025-03-08 18:13:18 -0600

    Update

--- mega-brave/tools/ChocolateyInstall.ps1
+++ mega-brave/tools/ChocolateyInstall.ps1
@@ -4,7 +4,7 @@ $packageName = 'mega-chrome'
 $extensionID = 'bigefpfhnfcobdlfbedofhhaibnlghod'
 if ($bits -eq 64) {
     if (Test-Path -Path "HKLM:\SOFTWARE\Wow6432node\BraveSoftware\Brave\Extensions\$extensionID") {
-        Write-Host "Extension already installed." -foreground "magenta" –backgroundcolor "blue"
+        Write-Host "Extension already installed." -foreground "magenta" -backgroundcolor "blue"
     } else {
         New-Item -Force -Path "HKLM:\SOFTWARE\Wow6432node\BraveSoftware\Brave\Extensions\$extensionID" | out-null
         New-ItemProperty -Path "HKLM:\SOFTWARE\Wow6432node\BraveSoftware\Brave\Extensions\$extensionID\" -Name "update_url" -Value "https://clients2.google.com/service/update2/crx" | out-null
