ad136c5c453d5fb65889346ccc212922f9364885
asheroto <49938263+asheroto@users.noreply.github.com>
2025-05-19 11:25:19 -0500

    Move 11th Gen, add 14th-15th gen

--- intel-rst-driver/tools/ChocolateyInstall.ps1
+++ intel-rst-driver/tools/ChocolateyInstall.ps1
@@ -53,17 +53,23 @@ switch ($gen) {
         $url = 'https://downloadmirror.intel.com/773230/SetupRST.exe'
         $checksum = 'BC077F9C784305330DF66195FF8C96FCE753B1E370189A6E036005B9E589F7AD'
     }
-    { $_ -in 10 } {
+    { $_ -in 10, 11 } {
         $infoUrl = 'https://www.intel.com/content/www/us/en/download/19512/intel-rapid-storage-technology-driver-installation-software-with-intel-optane-memory-10th-and-11th-gen-platforms.html'
         $version = '18.7.6.1010.3'
         $url = 'https://downloadmirror.intel.com/773229/SetupRST.exe'
         $checksum = 'A2B2E20D6D8100E9EE344746F80849524C64490B90686A13C09268CADB976B37'
     }
-    { $_ -in 11, 12, 13 } {
-        $infoUrl = 'https://www.intel.com/content/www/us/en/download/720755/intel-rapid-storage-technology-driver-installation-software-with-intel-optane-memory-11th-and-12th-gen-platforms.html'
-        $version = '19.5.2.1049.5'
-        $url = 'https://downloadmirror.intel.com/751275/SetupRST.exe'
-        $checksum = 'F671E1CBBD4313C40878A1AD6F4152585D0E10192CBAD39274E9D5EBE7276FE7'
+    { $_ -in 12, 13 } {
+        $infoUrl = 'https://www.intel.com/content/www/us/en/download/849933/intel-rapid-storage-technology-driver-installation-software-with-intel-optane-memory-12th-to-13th-gen-platforms.html'
+        $version = '19.5.8.1059.2'
+        $url = 'https://downloadmirror.intel.com/849934/SetupRST.exe'
+        $checksum = '8AF488ECD76258A0A539ADA28098A2961472D670572900AED216F48C712BC45B'
+    }
+    { $_ -in 14, 15 } {
+        $infoUrl = 'https://www.intel.com/content/www/us/en/download/849936/intel-rapid-storage-technology-driver-installation-software-with-intel-optane-memory-12th-to-15th-gen-platforms.html'
+        $version = '20.2.1.1016.4'
+        $url = 'https://downloadmirror.intel.com/849939/SetupRST.exe'
+        $checksum = '700C403E23AE3F7CEF9581A431D8F6DCB8C6A48F895725E114131C3A29232C99'
     }
     default {
         throw "Unsupported processor generation $gen."
