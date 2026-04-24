135ecfe75a15cdbb9a0910d442804475f4ba3876
asheroto <49938263+asheroto@users.noreply.github.com>
2026-04-24 11:29:33 -0500

    Fix HP

--- hp-universal-print-driver-pcl/tools/ChocolateyInstall.ps1
+++ hp-universal-print-driver-pcl/tools/ChocolateyInstall.ps1
@@ -11,7 +11,7 @@ $version = "7.9.0.26347"
 $packageArgs = @{
 	packageName    = 'hp-universal-print-driver-pcl'
 
-	url            = "https://ftp.hp.com/pub/softlib/software13/UPD/upd-pcl6-x64-${version}.zip"
+	url            = "https://ftp.hp.com/pub/softlib/software13/printers/UPD/upd-pcl6-x64-${version}.zip"
 	checksum       = '248EA75AA33F94A680D445C0CCA1787D335C49583E2F0F36933C7C3E7A93E98A'
 	softwareName   = 'HP Universal Printing PCL 6'
 	fileLocation   = "$toolsDir\unzippedfiles\install.exe"
