02bc2e3f3770c043de69ce0b5202a8c1c1ee0537
asheroto <49938263+asheroto@users.noreply.github.com>
2026-05-08 18:18:14 -0500

    Fix MessagesForWeb

--- MessagesForWeb/tools/ChocolateyInstall.ps1
+++ MessagesForWeb/tools/ChocolateyInstall.ps1
@@ -1,23 +1,20 @@
 $ErrorActionPreference = 'Stop'
 
 $packageName = 'MessagesForWeb'
-$softwareName = 'Messages for Web'
-$version = '1.0.4'
-$url = "https://github.com/asheroto/MessagesForWeb/releases/download/${version}/MessagesForWeb.exe"
-$checksum = 'BBE771B74EDE62BB0E119A4949A698EC6E92377F4419B5F9F51521D8C9E23F8D'
+$version = '1.0.5'
+$url = "https://github.com/asheroto/MessagesForWeb/releases/download/$version/MessagesForWeb.exe"
+$checksum = '8724D32B8B1B949F1BACE1E7BBE0FEE55A0DED2C46FAAC317FB17C98EEB79BEC'
 $silentArgs = '/quiet'
 $validExitCodes = @(0)
 
 $packageArgs = @{
     packageName    = $packageName
     fileType       = 'exe'
-    file           = $fileLocation
     url            = $url
     checksum       = $checksum
     checksumType   = 'sha256'
     silentArgs     = $silentArgs
     validExitCodes = $validExitCodes
-    softwareName   = $softwareName
 }
 
 # Install the package
--- MessagesForWeb/tools/ChocolateyUninstall.ps1
+++ MessagesForWeb/tools/ChocolateyUninstall.ps1
@@ -7,21 +7,12 @@ $key = Get-UninstallRegistryKey -SoftwareName "$softwareName*" | Select-Object -
 # Exit if the package is already uninstalled
 if (-not $key) { return $null }
 
-$uninstallString = $key.UninstallString
-
-# Check if silent uninstall arguments are already included
-if ($uninstallString -notmatch '/qn') {
-    $silentArgs = $uninstallString.Replace("MsiExec.exe /X", "").Trim() + " /qn"
-} else {
-    $silentArgs = $uninstallString
-}
-
 $packageArgs = @{
     packageName    = $packageName
-    file           = 'MsiExec.exe'
-    silentArgs     = $silentArgs
     fileType       = 'msi'
-    validExitCodes = @(0)
+    file           = ($key.UninstallString -replace 'MsiExec\.exe /X', '')
+    silentArgs     = '/qn /norestart'
+    validExitCodes = @(0, 3010, 1605, 1614, 1641)
 }
 
 Uninstall-ChocolateyPackage @packageArgs
