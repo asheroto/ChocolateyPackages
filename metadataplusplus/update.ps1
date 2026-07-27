3a44bac039a13b95de32763bb34240b4bdd6b465
asheroto <49938263+asheroto@users.noreply.github.com>
2026-07-27 15:02:30 -0500

    Update

--- metadataplusplus/update.ps1
+++ metadataplusplus/update.ps1
@@ -17,7 +17,7 @@ $packageInfo = @{
     FileUrl                  = 'SCRAPE'                                             # Set to SCRAPE if using DownloadUrlScrapePattern
     AutoPush                 = $true
     EnvFilePath              = "..\.env"
-    IgnoreVersion            = '2.5.2'
+    IgnoreVersion            = '3.0.1.20260727'
 }
 
 # Call the UpdateChocolateyPackage function and pass the hash table
